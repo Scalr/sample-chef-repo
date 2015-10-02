b705487da40dc429c01bb4f9351d825cf377fa99
Eugeny Kurkovich <stunko@scalr.com>
2015-10-02 18:03:30 +0300

    Added cookbook revizor-chef

---
+++ cookbooks/revizor-chef/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Scalr Inc."
+maintainer_email "stunko@scalr.com"
+name             "revizor-chef"
+license          "All rights reserved"
+description      "Cookbook for verifying chef-solo in Scalr"
+version          "0.1.0"
---
+++ cookbooks/revizor-chef/recipes/default.rb
@@ -0,0 +1,6 @@
+file "/root/chef_solo_result" do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+end
