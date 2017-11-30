9e885b1abacbdf645b8e3a968a8cc95789f8ba14
Nazar Ner <n.ner@scalr.com>
2017-11-30 15:46:45 +0200

    Added windows path to revizor-chef cookbook

--- cookbooks/revizor-chef/recipes/default.rb
+++ cookbooks/revizor-chef/recipes/default.rb
@@ -1,9 +1,23 @@
 if node['chef-solo']['result'].include? 'fail'
     raise "Special test exception"
 end
-file "/root/#{node['chef-solo']['result']}" do
-    owner "root"
-    group "root"
-    mode "0755"
-    action :create
+case node['platform']
+when 'windows'
+    file "C:/#{node['chef-solo']['result']}" do
+        action :create
+    end
+else
+    file "/root/#{node['chef-solo']['result']}" do
+        owner "root"
+        group "root"
+        mode "0755"
+        action :create
+    end
+end
+
+ruby_block "show env" do
+    block do
+	    p ENV
+    end
+    action :run
 end
